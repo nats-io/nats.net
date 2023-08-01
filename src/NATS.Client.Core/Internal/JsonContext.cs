cb5e85a12982c2023726fc9df8a8eeeec8e79d9b
jasperd <jasper-d@users.noreply.github.com>
2023-08-01 18:16:57 +0200

    Remove defaults from src-gen options

--- src/NATS.Client.Core/Internal/JsonContext.cs
+++ src/NATS.Client.Core/Internal/JsonContext.cs
@@ -2,12 +2,7 @@
 
 namespace NATS.Client.Core.Internal;
 
-[JsonSourceGenerationOptions(
-    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-    IgnoreReadOnlyFields = false,
-    IgnoreReadOnlyProperties = false,
-    IncludeFields = false,
-    WriteIndented = false)]
+[JsonSourceGenerationOptions(DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
 [JsonSerializable(typeof(ServerInfo), GenerationMode = JsonSourceGenerationMode.Metadata)]
 [JsonSerializable(typeof(ClientOptions), GenerationMode = JsonSourceGenerationMode.Serialization)]
 internal sealed partial class JsonContext : JsonSerializerContext
