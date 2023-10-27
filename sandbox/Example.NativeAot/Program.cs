bd84448f4846d366ee0ba535d713612a64ce2468
Ziya Suzen <ziya@suzen.net>
2023-10-27 15:37:57 +0100

    Updated working example using native AOT

--- sandbox/Example.NativeAot/Program.cs
+++ sandbox/Example.NativeAot/Program.cs
@@ -1,13 +1,35 @@
-﻿using NATS.Client.Core;
+﻿using System.Text.Json.Serialization;
+using NATS.Client.Core;
 
 await using var nats = new NatsConnection();
 
-await using var sub = await nats.SubscribeAsync<int>("foo");
+await using var sub = await nats.SubscribeAsync<MyData>("foo", opts: new NatsSubOpts { Serializer = new NatsJsonContextSerializer(MyJsonContext.Default) });
 
 await nats.PingAsync();
 
-await nats.PublishAsync("foo", 1);
+await nats.PublishAsync(
+    "foo",
+    new MyData
+    {
+        Id = 1, Name = "bar",
+    },
+    opts: new NatsPubOpts
+    {
+        Serializer = new NatsJsonContextSerializer(MyJsonContext.Default),
+    });
 
 var msg = await sub.Msgs.ReadAsync();
 
 Console.WriteLine(msg.Data);
+
+public record MyData
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+}
+
+[JsonSerializable(typeof(MyData))]
+internal partial class MyJsonContext : JsonSerializerContext;
--- sandbox/Example.NativeAot/run.sh
+++ sandbox/Example.NativeAot/run.sh
@@ -1,2 +1,3 @@
+rm -rf ./dist
 dotnet publish -r linux-x64 -c Release -o dist
 ./dist/Example.NativeAot
