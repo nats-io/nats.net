3625e2ff38a6857e1e0f6e55ac7ffdd2e223adaa
Ziya Suzen <ziya@suzen.net>
2023-10-31 11:56:13 +0000

    Removed unused buffer interface
    Also removed method inlining on getters (based on Jasper's advice:
    Presumably JIT should always inline simple field accesses and the
    other getters look like they are likely inlined without
    AggressiveInlining when on a hot path.)
    
    Thank you @jasper-d

--- src/NATS.Client.Core/NatsBufferWriter.cs
+++ src/NATS.Client.Core/NatsBufferWriter.cs
@@ -9,51 +9,10 @@ using BitOperations = System.Numerics.BitOperations;
 namespace NATS.Client.Core;
 
 /// <summary>
-/// An interface that expands <see cref="IBufferWriter{T}"/> with the ability to also inspect
-/// the written data, and to reset the underlying buffer to write again from the start.
-/// </summary>
-/// <typeparam name="T">The type of items in the current buffer.</typeparam>
-public interface INatsBuffer<T> : IBufferWriter<T>
-{
-    /// <summary>
-    /// Gets the data written to the underlying buffer so far, as a <see cref="ReadOnlyMemory{T}"/>.
-    /// </summary>
-    ReadOnlyMemory<T> WrittenMemory { get; }
-
-    /// <summary>
-    /// Gets the data written to the underlying buffer so far, as a <see cref="ReadOnlySpan{T}"/>.
-    /// </summary>
-    ReadOnlySpan<T> WrittenSpan { get; }
-
-    /// <summary>
-    /// Gets the amount of data written to the underlying buffer so far.
-    /// </summary>
-    int WrittenCount { get; }
-
-    /// <summary>
-    /// Gets the total amount of space within the underlying buffer.
-    /// </summary>
-    int Capacity { get; }
-
-    /// <summary>
-    /// Gets the amount of space available that can still be written into without forcing the underlying buffer to grow.
-    /// </summary>
-    int FreeCapacity { get; }
-
-    /// <summary>
-    /// Clears the data written to the underlying buffer.
-    /// </summary>
-    /// <remarks>
-    /// You must clear the <see cref="INatsBuffer{T}"/> instance before trying to re-use it.
-    /// </remarks>
-    void Clear();
-}
-
-/// <summary>
 /// Represents a heap-based, array-backed output sink into which <typeparamref name="T"/> data can be written.
 /// </summary>
 /// <typeparam name="T">The type of items to write to the current instance.</typeparam>
-public sealed class NatsBufferWriter<T> : INatsBuffer<T>, IMemoryOwner<T>
+public sealed class NatsBufferWriter<T> : IBufferWriter<T>, IMemoryOwner<T>
 {
     /// <summary>
     /// The default buffer size to use to expand empty arrays.
@@ -139,10 +98,11 @@ public sealed class NatsBufferWriter<T> : INatsBuffer<T>, IMemoryOwner<T>
         get => MemoryMarshal.AsMemory(WrittenMemory);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Gets the data written to the underlying buffer so far, as a <see cref="ReadOnlyMemory{T}"/>.
+    /// </summary>
     public ReadOnlyMemory<T> WrittenMemory
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
         {
             var array = _array;
@@ -156,10 +116,11 @@ public sealed class NatsBufferWriter<T> : INatsBuffer<T>, IMemoryOwner<T>
         }
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Gets the data written to the underlying buffer so far, as a <see cref="ReadOnlySpan{T}"/>.
+    /// </summary>
     public ReadOnlySpan<T> WrittenSpan
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
         {
             var array = _array;
@@ -173,17 +134,19 @@ public sealed class NatsBufferWriter<T> : INatsBuffer<T>, IMemoryOwner<T>
         }
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Gets the amount of data written to the underlying buffer so far.
+    /// </summary>
     public int WrittenCount
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => _index;
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Gets the total amount of space within the underlying buffer.
+    /// </summary>
     public int Capacity
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
         {
             var array = _array;
@@ -197,10 +160,11 @@ public sealed class NatsBufferWriter<T> : INatsBuffer<T>, IMemoryOwner<T>
         }
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Gets the amount of space available that can still be written into without forcing the underlying buffer to grow.
+    /// </summary>
     public int FreeCapacity
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
         {
             var array = _array;
@@ -214,7 +178,12 @@ public sealed class NatsBufferWriter<T> : INatsBuffer<T>, IMemoryOwner<T>
         }
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Clears the data written to the underlying buffer.
+    /// </summary>
+    /// <remarks>
+    /// You must clear the <see cref="INatsBuffer{T}"/> instance before trying to re-use it.
+    /// </remarks>
     public void Clear()
     {
         var array = _array;
