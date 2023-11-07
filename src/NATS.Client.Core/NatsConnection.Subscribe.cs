81634ec488ce63a010438676b6bf0c58d997a4c8
Ziya Suzen <ziya@synadia.com>
2023-11-07 21:48:02 +0000

    Async iterator subscription memory fix (#188)
    The issue was happening with Perf tests hanging. It turns out when
    subscription was handled within async iterator we loose a reference to
    the subscription potentially because it's in a using as well. This
    solution makes sure the connection object will keep a reference
    so subscription object won't be collected because of the weak ref
    in the subscription manager. Also we don't need to worry about
    disposing the subscription now because as soon as the loop exits
    it will be disposed.

--- src/NATS.Client.Core/NatsConnection.Subscribe.cs
+++ src/NATS.Client.Core/NatsConnection.Subscribe.cs
@@ -1,22 +1,43 @@
+using System.Collections.Concurrent;
 using System.Runtime.CompilerServices;
 
 namespace NATS.Client.Core;
 
 public partial class NatsConnection
 {
+    // Keep subscription alive until the channel reader completes.
+    // Otherwise subscription is collected because subscription manager
+    // only holds a weak reference to it.
+    private readonly ConcurrentDictionary<long, NatsSubBase> _subAnchor = new();
+    private long _subAnchorId;
+
     /// <inheritdoc />
     public async IAsyncEnumerable<NatsMsg<T>> SubscribeAsync<T>(string subject, string? queueGroup = default, INatsDeserialize<T>? serializer = default, NatsSubOpts? opts = default, [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        await using var sub = await SubscribeInternalAsync(subject, queueGroup, serializer, opts, cancellationToken).ConfigureAwait(false);
-
-        // We don't cancel the channel reader here because we want to keep reading until the subscription
-        // channel writer completes so that messages left in the channel can be consumed before exit the loop.
-        while (await sub.Msgs.WaitToReadAsync(CancellationToken.None).ConfigureAwait(false))
+        var anchor = Interlocked.Increment(ref _subAnchorId);
+        try
         {
-            while (sub.Msgs.TryRead(out var msg))
+            serializer ??= Opts.SerializerRegistry.GetDeserializer<T>();
+
+            await using var sub = new NatsSub<T>(this, SubscriptionManager.GetManagerFor(subject), subject, queueGroup, opts, serializer, cancellationToken);
+
+            _subAnchor[anchor] = sub;
+
+            await SubAsync(sub, cancellationToken: cancellationToken).ConfigureAwait(false);
+
+            // We don't cancel the channel reader here because we want to keep reading until the subscription
+            // channel writer completes so that messages left in the channel can be consumed before exit the loop.
+            while (await sub.Msgs.WaitToReadAsync(CancellationToken.None).ConfigureAwait(false))
             {
-                yield return msg;
+                while (sub.Msgs.TryRead(out var msg))
+                {
+                    yield return msg;
+                }
             }
+        }
+        finally
+        {
+            _subAnchor.TryRemove(anchor, out _);
         }
     }
 
