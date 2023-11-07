0aae90b1e0b5bf710948c6950772256a733e7ec2
Ziya Suzen <ziya@suzen.net>
2023-11-07 13:57:13 +0000

    Hide subscribe internal method
    SubscribeInternalAsync() method was left public by mistake.

--- src/NATS.Client.Core/NatsConnection.Subscribe.cs
+++ src/NATS.Client.Core/NatsConnection.Subscribe.cs
@@ -20,7 +20,7 @@ public partial class NatsConnection
         }
     }
 
-    public async ValueTask<NatsSub<T>> SubscribeInternalAsync<T>(string subject, string? queueGroup = default, INatsDeserialize<T>? serializer = default, NatsSubOpts? opts = default, CancellationToken cancellationToken = default)
+    internal async ValueTask<NatsSub<T>> SubscribeInternalAsync<T>(string subject, string? queueGroup = default, INatsDeserialize<T>? serializer = default, NatsSubOpts? opts = default, CancellationToken cancellationToken = default)
     {
         serializer ??= Opts.SerializerRegistry.GetDeserializer<T>();
         var sub = new NatsSub<T>(this, SubscriptionManager.GetManagerFor(subject), subject, queueGroup, opts, serializer, cancellationToken);
