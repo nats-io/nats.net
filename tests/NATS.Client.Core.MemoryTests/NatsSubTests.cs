357691515cfa814512b4872fd1033f538412245c
Ziya Suzen <ziya@suzen.net>
2023-11-07 17:42:23 +0000

    Async iterator subscription memory fix
    The issue was happening with Perf tests hanging. It turns out when
    subscription was handled within async iterator we loose a reference to
    the subscription potentially because it's in a using as well. This
    solution makes sure the connection object will keep a reference
    so subscription object won't be collected because of the weak ref
    in the subscription manager. Also we don't need to worry about
    disposing the subscription now because as soon as the loop exits
    it will be disposed.

--- tests/NATS.Client.Core.MemoryTests/NatsSubTests.cs
+++ tests/NATS.Client.Core.MemoryTests/NatsSubTests.cs
@@ -35,4 +35,78 @@ public class NatsSubTests
             server.DisposeAsync().GetAwaiter().GetResult();
         }
     }
+
+    [Test]
+    public void Subscription_should_not_be_collected_when_in_async_enumerator()
+    {
+        var server = NatsServer.Start();
+        try
+        {
+            var nats = server.CreateClientConnection();
+
+            var sync = 0;
+
+            var sub = Task.Run(async () =>
+            {
+                var count = 0;
+                await foreach (var msg in nats.SubscribeAsync<string>("foo.*"))
+                {
+                    if (msg.Subject == "foo.sync")
+                    {
+                        Interlocked.Increment(ref sync);
+                        continue;
+                    }
+
+                    if (++count == 10)
+                        break;
+                }
+            });
+
+
+            var pub = Task.Run(async () =>
+            {
+                while (Volatile.Read(ref sync) == 0)
+                {
+                    await nats.PublishAsync("foo.sync", "sync");
+                }
+
+                for (var i = 0; i < 10; i++)
+                {
+                    GC.Collect();
+
+                    dotMemory.Check(memory =>
+                    {
+                        var count = memory.GetObjects(where => where.Type.Is<NatsSub<string>>()).ObjectsCount;
+                        Assert.That(count, Is.EqualTo(1), "Alive");
+                    });
+
+                    await nats.PublishAsync("foo.data", "data");
+                }
+            });
+
+            var waitPub = Task.WaitAll(new[] { pub }, TimeSpan.FromSeconds(10));
+            if (!waitPub)
+            {
+                Assert.Fail("Timed out waiting for pub task to complete");
+            }
+
+            var waitSub = Task.WaitAll(new[] { sub }, TimeSpan.FromSeconds(10));
+            if (!waitSub)
+            {
+                Assert.Fail("Timed out waiting for sub task to complete");
+            }
+
+            GC.Collect();
+
+            dotMemory.Check(memory =>
+            {
+                var count = memory.GetObjects(where => where.Type.Is<NatsSub<string>>()).ObjectsCount;
+                Assert.That(count, Is.EqualTo(0), "Collected");
+            });
+        }
+        finally
+        {
+            server.DisposeAsync().GetAwaiter().GetResult();
+        }
+    }
 }
