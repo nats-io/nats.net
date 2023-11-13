240a7e545d57cf843b3f3f16fdca08175287050e
Ziya Suzen <ziya@synadia.com>
2023-11-13 13:17:01 +0000

    JetStream ordered consume stability fixes (#202)
    * JetStream ordered consume stability fixes
    
    Because ordered consume operations use ephemeral consumers it's important
    to be able to create new consumers when things go wrong. There are two
    major events that might strongly indicate we won't be able to find our
    consumer on the server: disconnects and idle heartbeat timeouts.
    
    With this approach introduced with this fix, we recreate the consumer on
    server disconnects and idle heartbeat timeouts, making sure the consumer
    can carry on from where it's left off (sequence state is maintained when
    this happens).
    
    Justification for large code duplication: NatsJSOrderedConsume.cs is a
    copy of NatsJSConsume.cs subscription class. Reason for this is to
    maintain stability of the normal consumer since the main behaviour is
    fairly different. There is also a chance the behaviours might diverge
    even greater as we discover other issues. We may consider to merge
    these classes in a tidy-up effort later on.
    
    We also introduced a general timeout (same as connection request timeout)
    for all JetStream API calls. We needed this because the consumer deletion
    process was sometimes hanging due to the server receiving the request
    being killed and never sending a response back. Before this we were
    relying on the CommandTimeout (which is 1 minute by default) to kick in.
    Now we use RequestTimeout (5 seconds by default) on the subscription
    waiting for the reply.
    
    * Consume memory test fixes
    
    * Ordered consumer create retries

---
+++ src/NATS.Client.JetStream/Internal/NatsJSOrderedConsume.cs
@@ -0,0 +1,408 @@
+using System.Buffers;
+using System.Text;
+using System.Threading.Channels;
+using Microsoft.Extensions.Logging;
+using NATS.Client.Core;
+using NATS.Client.Core.Commands;
+using NATS.Client.JetStream.Models;
+
+namespace NATS.Client.JetStream.Internal;
+
+internal class NatsJSOrderedConsume<TMsg> : NatsSubBase
+{
+    private readonly ILogger _logger;
+    private readonly bool _debug;
+    private readonly Channel<NatsJSMsg<TMsg>> _userMsgs;
+    private readonly Channel<PullRequest> _pullRequests;
+    private readonly NatsJSContext _context;
+    private readonly string _stream;
+    private readonly string _consumer;
+    private readonly CancellationToken _cancellationToken;
+    private readonly INatsDeserialize<TMsg> _serializer;
+    private readonly Timer _timer;
+    private readonly Task _pullTask;
+
+    private readonly long _maxMsgs;
+    private readonly long _expires;
+    private readonly long _idle;
+    private readonly long _hbTimeout;
+    private readonly long _thresholdMsgs;
+    private readonly long _maxBytes;
+    private readonly long _thresholdBytes;
+
+    private readonly object _pendingGate = new();
+    private long _pendingMsgs;
+    private long _pendingBytes;
+    private int _disposed;
+
+    public NatsJSOrderedConsume(
+        long maxMsgs,
+        long thresholdMsgs,
+        long maxBytes,
+        long thresholdBytes,
+        TimeSpan expires,
+        TimeSpan idle,
+        NatsJSContext context,
+        string stream,
+        string consumer,
+        string subject,
+        string? queueGroup,
+        INatsDeserialize<TMsg> serializer,
+        NatsSubOpts? opts,
+        CancellationToken cancellationToken)
+        : base(context.Connection, context.Connection.SubscriptionManager, subject, queueGroup, opts)
+    {
+        _cancellationToken = cancellationToken;
+        _logger = Connection.Opts.LoggerFactory.CreateLogger<NatsJSConsume<TMsg>>();
+        _debug = _logger.IsEnabled(LogLevel.Debug);
+        _context = context;
+        _stream = stream;
+        _consumer = consumer;
+        _serializer = serializer;
+
+        _maxMsgs = maxMsgs;
+        _thresholdMsgs = thresholdMsgs;
+        _maxBytes = maxBytes;
+        _thresholdBytes = thresholdBytes;
+        _expires = expires.ToNanos();
+        _idle = idle.ToNanos();
+        _hbTimeout = (int)(idle * 2).TotalMilliseconds;
+
+        if (_debug)
+        {
+            _logger.LogDebug(
+                NatsJSLogEvents.Config,
+                "Consume setup {@Config}",
+                new
+                {
+                    maxMsgs,
+                    thresholdMsgs,
+                    maxBytes,
+                    thresholdBytes,
+                    expires,
+                    idle,
+                    _hbTimeout,
+                });
+        }
+
+        _timer = new Timer(
+            static state =>
+            {
+                var self = (NatsJSOrderedConsume<TMsg>)state!;
+                self._logger.LogWarning("Idle heartbeat timeout");
+                self._userMsgs.Writer.TryComplete(new NatsJSTimeoutException("idle-heartbeat"));
+            },
+            this,
+            Timeout.Infinite,
+            Timeout.Infinite);
+
+        // Keep user channel small to avoid blocking the user code
+        // when disposed otherwise channel reader will continue delivering messages
+        // if there are messages queued up already. This channel is used to pass messages
+        // to the user from the subscription channel (which should be set to a
+        // sufficiently large value to avoid blocking socket reads in the
+        // NATS connection).
+        _userMsgs = Channel.CreateBounded<NatsJSMsg<TMsg>>(1);
+        Msgs = _userMsgs.Reader;
+
+        // Capacity as 1 is enough here since it's used for signaling only.
+        _pullRequests = Channel.CreateBounded<PullRequest>(1);
+        _pullTask = Task.Run(PullLoop);
+
+        ResetPending();
+
+        _context.Connection.ConnectionDisconnected += ConnectionOnConnectionDisconnected;
+    }
+
+    public ChannelReader<NatsJSMsg<TMsg>> Msgs { get; }
+
+    public ValueTask CallMsgNextAsync(string origin, ConsumerGetnextRequest request, CancellationToken cancellationToken = default)
+    {
+        if (_cancellationToken.IsCancellationRequested)
+            return default;
+
+        if (_debug)
+        {
+            _logger.LogDebug("Sending pull request for {Origin} {Msgs}, {Bytes}", origin, request.Batch, request.MaxBytes);
+        }
+
+        return Connection.PubModelAsync(
+            subject: $"{_context.Opts.Prefix}.CONSUMER.MSG.NEXT.{_stream}.{_consumer}",
+            data: request,
+            serializer: NatsJSJsonSerializer<ConsumerGetnextRequest>.Default,
+            replyTo: Subject,
+            headers: default,
+            cancellationToken);
+    }
+
+    public void ResetHeartbeatTimer() => _timer.Change(_hbTimeout, Timeout.Infinite);
+
+    public override async ValueTask DisposeAsync()
+    {
+        Interlocked.Exchange(ref _disposed, 1);
+
+        _context.Connection.ConnectionDisconnected -= ConnectionOnConnectionDisconnected;
+
+        await base.DisposeAsync().ConfigureAwait(false);
+        await _pullTask.ConfigureAwait(false);
+        await _timer.DisposeAsync().ConfigureAwait(false);
+    }
+
+    internal override IEnumerable<ICommand> GetReconnectCommands(int sid)
+    {
+        // Override normal subscription behavior to resubscribe on reconnect
+       yield break;
+    }
+
+    protected override async ValueTask ReceiveInternalAsync(
+        string subject,
+        string? replyTo,
+        ReadOnlySequence<byte>? headersBuffer,
+        ReadOnlySequence<byte> payloadBuffer)
+    {
+        ResetHeartbeatTimer();
+
+        if (subject == Subject)
+        {
+            if (headersBuffer.HasValue)
+            {
+                var headers = new NatsHeaders();
+                if (Connection.HeaderParser.ParseHeaders(new SequenceReader<byte>(headersBuffer.Value), headers))
+                {
+                    if (_maxBytes == 0 && headers.TryGetValue("Nats-Pending-Messages", out var natsPendingMsgs))
+                    {
+                        if (long.TryParse(natsPendingMsgs, out var pendingMsgs))
+                        {
+                            lock (_pendingGate)
+                            {
+                                if (_debug)
+                                {
+                                    _logger.LogDebug(
+                                        NatsJSLogEvents.PendingCount,
+                                        "Header pending messages current {Pending}",
+                                        _pendingMsgs);
+                                }
+
+                                _pendingMsgs -= pendingMsgs;
+                                if (_pendingMsgs < 0)
+                                    _pendingMsgs = 0;
+
+                                if (_debug)
+                                {
+                                    _logger.LogDebug(
+                                        NatsJSLogEvents.PendingCount,
+                                        "Header pending messages {Header} {Pending}",
+                                        natsPendingMsgs,
+                                        _pendingMsgs);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            _logger.LogError(NatsJSLogEvents.PendingCount, "Can't parse Nats-Pending-Messages {Header}", natsPendingMsgs);
+                        }
+                    }
+
+                    if (_maxBytes > 0 && headers.TryGetValue("Nats-Pending-Bytes", out var natsPendingBytes))
+                    {
+                        if (long.TryParse(natsPendingBytes, out var pendingBytes))
+                        {
+                            lock (_pendingGate)
+                            {
+                                if (_debug)
+                                {
+                                    _logger.LogDebug(NatsJSLogEvents.PendingCount, "Header pending bytes current {Pending}", _pendingBytes);
+                                }
+
+                                _pendingBytes -= pendingBytes;
+                                if (_pendingBytes < 0)
+                                    _pendingBytes = 0;
+
+                                if (_debug)
+                                {
+                                    _logger.LogDebug(NatsJSLogEvents.PendingCount, "Header pending bytes {Header} {Pending}", natsPendingBytes, _pendingBytes);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            _logger.LogError(NatsJSLogEvents.PendingCount, "Can't parse Nats-Pending-Bytes {Header}", natsPendingBytes);
+                        }
+                    }
+
+                    if (headers is { Code: 408, Message: NatsHeaders.Messages.RequestTimeout })
+                    {
+                    }
+                    else if (headers is { Code: 409, Message: NatsHeaders.Messages.MessageSizeExceedsMaxBytes })
+                    {
+                    }
+                    else if (headers is { Code: 100, Message: NatsHeaders.Messages.IdleHeartbeat })
+                    {
+                    }
+                    else if (headers.Code == 409 && string.Equals(headers.MessageText, "Leadership Change", StringComparison.OrdinalIgnoreCase))
+                    {
+                        _logger.LogDebug(NatsJSLogEvents.LeadershipChange, "Leadership Change");
+                        lock (_pendingGate)
+                        {
+                            _pendingBytes = 0;
+                            _pendingMsgs = 0;
+                        }
+                    }
+                    else if (headers.HasTerminalJSError())
+                    {
+                        _userMsgs.Writer.TryComplete(new NatsJSProtocolException(headers.Code, headers.Message, headers.MessageText));
+                        EndSubscription(NatsSubEndReason.JetStreamError);
+                    }
+                    else
+                    {
+                        if (_debug)
+                        {
+                            _logger.LogDebug(NatsJSLogEvents.ProtocolMessage, "Protocol message: {Code} {Description}", headers.Code, headers.MessageText);
+                        }
+                    }
+                }
+                else
+                {
+                    _logger.LogError(
+                        NatsJSLogEvents.Headers,
+                        "Can't parse headers: {HeadersBuffer}",
+                        Encoding.ASCII.GetString(headersBuffer.Value.ToArray()));
+                    throw new NatsJSException("Can't parse headers");
+                }
+            }
+            else
+            {
+                throw new NatsJSException("No header found");
+            }
+        }
+        else
+        {
+            var msg = new NatsJSMsg<TMsg>(
+                NatsMsg<TMsg>.Build(
+                    subject,
+                    replyTo,
+                    headersBuffer,
+                    payloadBuffer,
+                    Connection,
+                    Connection.HeaderParser,
+                    _serializer),
+                _context);
+
+            lock (_pendingGate)
+            {
+                if (_pendingMsgs > 0)
+                    _pendingMsgs--;
+            }
+
+            if (_maxBytes > 0)
+            {
+                if (_debug)
+                    _logger.LogDebug(NatsJSLogEvents.MessageProperty, "Message size {Size}", msg.Size);
+
+                lock (_pendingGate)
+                {
+                    _pendingBytes -= msg.Size;
+                }
+            }
+
+            // Stop feeding the user if we are disposed.
+            // We need to exit as soon as possible.
+            if (Volatile.Read(ref _disposed) == 0)
+            {
+                // We can't pass cancellation token here because we need to hand
+                // the message to the user to be processed. Writer will be completed
+                // when the user calls Stop() or when the subscription is closed.
+                await _userMsgs.Writer.WriteAsync(msg).ConfigureAwait(false);
+            }
+        }
+
+        CheckPending();
+    }
+
+    protected override void TryComplete()
+    {
+        _pullRequests.Writer.TryComplete();
+        _userMsgs.Writer.TryComplete();
+    }
+
+    private void ConnectionOnConnectionDisconnected(object? sender, string e)
+    {
+        _logger.LogWarning("Disconnected {Reason}", e);
+        _userMsgs.Writer.TryComplete(new NatsJSConnectionException(e));
+    }
+
+    private void ResetPending()
+    {
+        lock (_pendingGate)
+        {
+            _pendingMsgs = _maxMsgs;
+            _pendingBytes = _maxBytes;
+        }
+    }
+
+    private void CheckPending()
+    {
+        lock (_pendingGate)
+        {
+            if (_maxBytes > 0 && _pendingBytes <= _thresholdBytes)
+            {
+                if (_debug)
+                    _logger.LogDebug(NatsJSLogEvents.PendingCount, "Check pending bytes {Pending}, {MaxBytes}", _pendingBytes, _maxBytes);
+
+                Pull("chk-bytes", _maxMsgs, _maxBytes - _pendingBytes);
+                ResetPending();
+            }
+            else if (_maxBytes == 0 && _pendingMsgs <= _thresholdMsgs && _pendingMsgs < _maxMsgs)
+            {
+                if (_debug)
+                    _logger.LogDebug(NatsJSLogEvents.PendingCount, "Check pending messages {Pending}, {MaxMsgs}", _pendingMsgs, _maxMsgs);
+
+                Pull("chk-msgs", _maxMsgs - _pendingMsgs, 0);
+                ResetPending();
+            }
+        }
+    }
+
+    private void CompleteStop()
+    {
+        if (_debug)
+        {
+            _logger.LogDebug(NatsJSLogEvents.Stopping, "No more pull requests or messages in-flight, stopping");
+        }
+
+        // Schedule on the thread pool to avoid potential deadlocks.
+        ThreadPool.UnsafeQueueUserWorkItem(
+            state =>
+            {
+                var self = (NatsJSOrderedConsume<TMsg>)state!;
+                self._userMsgs.Writer.TryComplete();
+                self.EndSubscription(NatsSubEndReason.None);
+            },
+            this);
+    }
+
+    private void Pull(string origin, long batch, long maxBytes) => _pullRequests.Writer.TryWrite(new PullRequest
+    {
+        Request = new ConsumerGetnextRequest
+        {
+            Batch = batch,
+            MaxBytes = maxBytes,
+            IdleHeartbeat = _idle,
+            Expires = _expires,
+        },
+        Origin = origin,
+    });
+
+    private async Task PullLoop()
+    {
+        await foreach (var pr in _pullRequests.Reader.ReadAllAsync().ConfigureAwait(false))
+        {
+            var origin = $"pull-loop({pr.Origin})";
+            await CallMsgNextAsync(origin, pr.Request).ConfigureAwait(false);
+            if (_debug)
+            {
+                _logger.LogDebug(NatsJSLogEvents.PullRequest, "Pull request issued for {Origin} {Batch}, {MaxBytes}", origin, pr.Request.Batch, pr.Request.MaxBytes);
+            }
+        }
+    }
+}
