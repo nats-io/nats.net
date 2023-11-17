23fed3d9f361ef8a09e29736dd2e66b70b91df03
Ziya Suzen <ziya@synadia.com>
2023-11-17 16:34:41 +0000

    JetStream options API prefix bug fix (#218)
    * JetStream options API prefix bug fix
    
    * Update test cases

---
+++ tests/NATS.Client.JetStream.Tests/OptionsTests.cs
@@ -0,0 +1,14 @@
+﻿namespace NATS.Client.JetStream.Tests;
+
+public class OptionsTests
+{
+    [Theory]
+    [InlineData(null, null, "$JS.API")]
+    [InlineData("OTHER", null, "OTHER")]
+    [InlineData(null, "ABC", "$JS.API.ABC")]
+    [InlineData("$JS.API", null, "$JS.API")]
+    [InlineData("$JS.API", "ABC", "$JS.API.ABC")]
+    [InlineData("OTHER", "ABC", "OTHER.ABC")]
+    public void Api_prefix(string? prefix, string? domain, string expected) =>
+        new NatsJSOpts(new NatsOpts(), apiPrefix: prefix, domain: domain).Prefix.Should().Be(expected);
+}
