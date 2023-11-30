38086a3de9166c458936ef139b6f4e48829eee1d
Ziya Suzen <ziya@suzen.net>
2023-11-30 23:56:21 +0000

    Test timeout increase

--- tests/NATS.Client.JetStream.Tests/ConsumerNotificationTest.cs
+++ tests/NATS.Client.JetStream.Tests/ConsumerNotificationTest.cs
@@ -87,7 +87,7 @@ public class ConsumerNotificationTest
         await using var nats = server.CreateClientConnection();
         var js = new NatsJSContext(nats);
 
-        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
 
         await js.CreateStreamAsync("s1", new[] { "s1.*" }, cts.Token);
 
