36bc100e121f80c921119e2dd4619bf6e02eb7e5
Ziya Suzen <ziya@suzen.net>
2023-12-01 00:44:10 +0000

    Use message interfaces only when required

--- src/NATS.Client.Core/NatsMsg.cs
+++ src/NATS.Client.Core/NatsMsg.cs
@@ -3,6 +3,77 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace NATS.Client.Core;
 
+public interface INatsMsg<T>
+{
+    bool IsNoRespondersError { get; }
+
+    /// <summary>The destination subject to publish to.</summary>
+    string Subject { get; init; }
+
+    /// <summary>The reply subject that subscribers can use to send a response back to the publisher/requester.</summary>
+    string? ReplyTo { get; init; }
+
+    /// <summary>Message size in bytes.</summary>
+    int Size { get; init; }
+
+    /// <summary>Pass additional information using name-value pairs.</summary>
+    NatsHeaders? Headers { get; init; }
+
+    /// <summary>Serializable data object.</summary>
+    T? Data { get; init; }
+
+    /// <summary>NATS connection this message is associated to.</summary>
+    INatsConnection? Connection { get; init; }
+
+    /// <summary>
+    /// Reply with an empty message.
+    /// </summary>
+    /// <param name="headers">Optional message headers.</param>
+    /// <param name="replyTo">Optional reply-to subject.</param>
+    /// <param name="opts">A <see cref="NatsPubOpts"/> for publishing options.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to cancel the command.</param>
+    /// <returns>A <see cref="ValueTask"/> that represents the asynchronous send operation.</returns>
+    ValueTask ReplyAsync(NatsHeaders? headers = default, string? replyTo = default, NatsPubOpts? opts = default, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Reply to this message.
+    /// </summary>
+    /// <param name="data">Serializable data object.</param>
+    /// <param name="headers">Optional message headers.</param>
+    /// <param name="replyTo">Optional reply-to subject.</param>
+    /// <param name="serializer">Serializer to use for the message type.</param>
+    /// <param name="opts">A <see cref="NatsPubOpts"/> for publishing options.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to cancel the command.</param>
+    /// <typeparam name="TReply">Specifies the type of data that may be sent to the NATS Server.</typeparam>
+    /// <returns>A <see cref="ValueTask"/> that represents the asynchronous send operation.</returns>
+    /// <remarks>
+    /// <para>
+    /// Publishes a new message using the reply-to subject from the this message as the destination subject.
+    /// </para>
+    /// <para>
+    /// If the <paramref name="serializer"/> is not specified, the <see cref="INatsSerializerRegistry"/> assigned to
+    /// the <see cref="NatsConnection"/> will be used to find a serializer for the type <typeparamref name="TReply"/>.
+    /// You can specify a <see cref="INatsSerializerRegistry"/> in <see cref="NatsOpts"/> when creating a
+    /// <see cref="NatsConnection"/>. If not specified, <see cref="NatsDefaultSerializerRegistry"/> will be used.
+    /// </para>
+    /// </remarks>
+    ValueTask ReplyAsync<TReply>(TReply data, NatsHeaders? headers = default, string? replyTo = default, INatsSerialize<TReply>? serializer = default, NatsPubOpts? opts = default, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Reply to this message.
+    /// </summary>
+    /// <param name="msg">A <see cref="NatsMsg{T}"/> representing message details.</param>
+    /// <param name="serializer">Serializer to use for the message type.</param>
+    /// <param name="opts">A <see cref="NatsPubOpts"/> for publishing options.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to cancel the command.</param>
+    /// <typeparam name="TReply">Specifies the type of data that may be sent to the NATS Server.</typeparam>
+    /// <returns>A <see cref="ValueTask"/> that represents the asynchronous send operation.</returns>
+    /// <remarks>
+    /// Publishes a new message using the reply-to subject from the this message as the destination subject.
+    /// </remarks>
+    ValueTask ReplyAsync<TReply>(NatsMsg<TReply> msg, INatsSerialize<TReply>? serializer = default, NatsPubOpts? opts = default, CancellationToken cancellationToken = default);
+}
+
 /// <summary>
 /// NATS message structure as defined by the protocol.
 /// </summary>
@@ -28,7 +99,7 @@ public readonly record struct NatsMsg<T>(
     int Size,
     NatsHeaders? Headers,
     T? Data,
-    INatsConnection? Connection)
+    INatsConnection? Connection) : INatsMsg<T>
 {
     public bool IsNoRespondersError => Headers?.Code == 503;
 
---
+++ tests/NATS.Client.JetStream.Tests/MessageInterfaceTest.cs
@@ -0,0 +1,49 @@
+using NATS.Client.Core.Tests;
+
+namespace NATS.Client.JetStream.Tests;
+
+public class MessageInterfaceTest
+{
+    [Fact]
+    public async Task Using_message_interface()
+    {
+        await using var server = NatsServer.StartJS();
+        await using var nats = server.CreateClientConnection();
+        var js = new NatsJSContext(nats);
+
+        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+
+        await js.CreateStreamAsync("s1", new[] { "s1.*" }, cts.Token);
+
+        var ack = await js.PublishAsync("s1.foo", "test_msg", cancellationToken: cts.Token);
+        ack.EnsureSuccess();
+
+        var consumer = await js.CreateConsumerAsync("s1", "c1", cancellationToken: cts.Token);
+
+        await foreach (var natsJSMsg in consumer.ConsumeAsync<string>(cancellationToken: cts.Token))
+        {
+            // Boxing allocation: conversion from 'NatsJSMsg<string>' to 'INatsJSMsg<string>' requires boxing of the value type
+            //                        vvvvvvvvv
+            await ProcessMessageAsync(natsJSMsg, cts.Token);
+            break;
+        }
+
+        await Retry.Until(
+            "ack pending 0",
+            async () =>
+            {
+                var c = await js.GetConsumerAsync("s1", "c1", cts.Token);
+                return c.Info.NumAckPending == 0;
+            },
+            retryDelay: TimeSpan.FromSeconds(1),
+            timeout: TimeSpan.FromSeconds(20));
+        await consumer.RefreshAsync(cts.Token);
+        Assert.Equal(0, consumer.Info.NumAckPending);
+    }
+
+    private async Task ProcessMessageAsync(INatsJSMsg<string> natsJSMsg, CancellationToken cancellationToken = default)
+    {
+        natsJSMsg.Data.Should().Be("test_msg");
+        await natsJSMsg.AckAsync(cancellationToken: cancellationToken);
+    }
+}
