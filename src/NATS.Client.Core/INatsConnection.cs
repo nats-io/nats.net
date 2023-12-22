4869e549cc51eeb50cb1c7984bfecef55cad4998
Stephen Spates <sspates@starbucks.com>
2023-12-22 12:32:04 -0800

    missed blank line between properties

--- src/NATS.Client.Core/INatsConnection.cs
+++ src/NATS.Client.Core/INatsConnection.cs
@@ -15,6 +15,7 @@ public interface INatsConnection : IAsyncDisposable
     NatsOpts Opts { get; }
 
     NatsConnectionState ConnectionState { get; }
+
     NatsHeaderParser HeaderParser { get; }
 
 
