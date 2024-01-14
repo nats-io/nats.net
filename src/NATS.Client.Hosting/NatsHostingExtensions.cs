f7c68ffd7d98ca3d6b64485bebcf90658e4d40ab
rickdotnet <rick@rickdot.net>
2024-01-14 10:59:25 -0600

    StyleCop suppression for conditional build

--- src/NATS.Client.Hosting/NatsHostingExtensions.cs
+++ src/NATS.Client.Hosting/NatsHostingExtensions.cs
@@ -11,6 +11,10 @@ public static class NatsHostingExtensions
     /// Add NatsConnection/Pool to ServiceCollection. When poolSize = 1, registered `NatsConnection` and `INatsConnection` as singleton.
     /// Others, registered `NatsConnectionPool` as singleton, `NatsConnection` and `INatsConnection` as transient(get from pool).
     /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1001:Commas should not be preceded by whitespace", Justification = "Required for conditional build.")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1009:Closing parenthesis should not be preceded by a space", Justification = "Required for conditional build.")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1111:Closing parenthesis should be on the same line as the last parameter", Justification = "Required for conditional build.")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1113:Comma should be on the same line as previous parameter", Justification = "Required for conditional build.")]
     public static IServiceCollection AddNats(
         this IServiceCollection services,
         int poolSize = 1,
