e90d588d9c2732ffd64729d177bdfe93646e9505
Ziya Suzen <ziya@suzen.net>
2024-01-22 17:09:43 +0000

    Send buffer refinements

---
+++ tests/NATS.Client.TestUtilities/InMemoryTestLoggerFactory.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace NATS.Client.TestUtilities;
+
+public class InMemoryTestLoggerFactory(LogLevel level) : ILoggerFactory
+{
+    private readonly List<LogMessage> _messages = new();
+
+    public IReadOnlyList<LogMessage> Logs
+    {
+        get
+        {
+            lock (_messages)
+                return _messages.ToList();
+        }
+    }
+
+    public ILogger CreateLogger(string categoryName) => new TestLogger(categoryName, level, this);
+
+    public void AddProvider(ILoggerProvider provider)
+    {
+    }
+
+    public void Dispose()
+    {
+    }
+
+    private void Log(string categoryName, LogLevel logLevel, EventId eventId, Exception? exception, string message)
+    {
+        lock (_messages)
+            _messages.Add(new LogMessage(categoryName, logLevel, eventId, exception, message));
+    }
+
+    public record LogMessage(string Category, LogLevel LogLevel, EventId EventId, Exception? Exception, string Message);
+
+    private class TestLogger(string categoryName, LogLevel level, InMemoryTestLoggerFactory logger) : ILogger
+    {
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            if (logLevel >= level)
+                logger.Log(categoryName, logLevel, eventId, exception, formatter(state, exception));
+        }
+
+        public bool IsEnabled(LogLevel logLevel) => logLevel >= level;
+
+        public IDisposable BeginScope<TState>(TState state) => new NullDisposable();
+
+        private class NullDisposable : IDisposable
+        {
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
