9b049d6408b0d6152109ab9810884adba94a685e
Ziya Suzen <ziya@synadia.com>
2024-01-30 17:41:07 +0000

    Added deliver_group to consumer config (#366)

---
+++ tests/NATS.Client.JetStream.Tests/ConsumerSetupTest.cs
@@ -0,0 +1,39 @@
+using NATS.Client.Core.Tests;
+using NATS.Client.JetStream.Models;
+
+namespace NATS.Client.JetStream.Tests;
+
+public class ConsumerSetupTest
+{
+    [Fact]
+    public async Task Create_push_consumer()
+    {
+        await using var server = NatsServer.StartJS();
+        await using var nats = server.CreateClientConnection();
+        var js = new NatsJSContext(nats);
+
+        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+        await js.CreateStreamAsync("s1", new[] { "s1.*" }, cts.Token);
+
+        await js.CreateOrUpdateConsumerAsync(
+            stream: "s1",
+            config: new ConsumerConfig
+            {
+                Name = "c1",
+                DeliverSubject = "i1",
+                DeliverGroup = "q1",
+            },
+            cancellationToken: cts.Token);
+
+        var consumer = await js.GetConsumerAsync("s1", "c1", cts.Token);
+
+        var info = consumer.Info;
+        Assert.Equal("s1", info.StreamName);
+
+        var config = info.Config;
+        Assert.Equal("c1", config.Name);
+        Assert.Equal("i1", config.DeliverSubject);
+        Assert.Equal("q1", config.DeliverGroup);
+    }
+}
