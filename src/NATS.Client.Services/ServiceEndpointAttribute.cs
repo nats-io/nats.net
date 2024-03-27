30e2e01bdf8aa125b9758d2cb36dcaba8361bbf0
Adam Reed <adam.reed@reedenterprising.com>
2024-03-27 18:53:11 -0500

    WIP

--- src/NATS.Client.Services/ServiceEndpointAttribute.cs
+++ src/NATS.Client.Services/ServiceEndpointAttribute.cs
@@ -1,7 +1,7 @@
 namespace NATS.Client.Services;
 
 [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
-public sealed class ServiceEndpointAttribute(string name, string group = null) : Attribute
+public sealed class NatsServiceEndpointAttribute(string name, string group = null) : Attribute
 {
     public string Name { get; } = name;
     public string Group { get; } = group;
--- tests/EndpointRegistrationTest/Program.cs
+++ tests/EndpointRegistrationTest/Program.cs
@@ -14,16 +14,41 @@ internal class Program
         var svc = new NatsSvcContext(nats);
         await using var testService = await svc.AddServiceAsync("test", "1.0.0");
 
+
+        IDictionary<string, string>? metadata = null;
+        INatsDeserialize<MyMessage>? serializer = null;
+        CancellationToken cancellationToken = default;
+
+        await testService.AddEndpointAsync<MyMessage>(
+            HandleIncomingMessageAsync,
+            name: "MyEndpoint",
+            subject: "my.subject",
+            queueGroup: "MyQueueGroup",
+            metadata: metadata,
+            serializer: serializer,
+            cancellationToken: cancellationToken
+        );
+
         var registrar = INatsSvcEndpointRegistrar.GetRegistrar();
         await registrar.RegisterEndpointsAsync(testService);
         
         Console.ReadLine();
     }
+
+    private static ValueTask HandleIncomingMessageAsync(NatsSvcMsg<MyMessage> arg)
+    {
+        throw new NotImplementedException();
+    }
 }
 
-public class MyClass
+internal class MyMessage
 {
-    [ServiceEndpoint("divide42", "math-group")]
+}
+
+[NatsServiceController]
+public class MyClass : NatsServiceControllerBase
+{
+    [NatsServiceEndpoint("divide42", "math-group")]
     public async Task<int> Divide42(int data)
     {
         if (data == 0)
@@ -34,6 +59,14 @@ public class MyClass
         return 42 / data;
     }
 
-    [ServiceEndpoint("getname", "name-group")]
+    [NatsServiceEndpoint("getname", "name-group")]
     public async Task<string> GetName(string input) => input + " " + input;
+}
+
+public class NatsServiceControllerAttribute : Attribute
+{
+}
+
+public class NatsServiceControllerBase
+{
 }
