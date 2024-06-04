97db5ad747ca57afa16deacea99d3756c50200ce
Caleb Lloyd <caleblloyd@gmail.com>
2024-06-04 09:20:47 -0400

    refactor memory tests to use Task.Run
    Signed-off-by: Caleb Lloyd <caleblloyd@gmail.com>

--- tests/NATS.Client.Core.MemoryTests/NatsSubTests.cs
+++ tests/NATS.Client.Core.MemoryTests/NatsSubTests.cs
@@ -38,130 +38,185 @@ public class NatsSubTests
         }
         finally
         {
-            server.DisposeAsync().GetAwaiter().GetResult();
+            server.DisposeAsync().AsTask().GetAwaiter().GetResult();
         }
     }
 
     [Test]
-    public void Subscription_should_not_be_collected_subscribe_async() =>
-        RunSubTest(async (nats, channelWriter, iterations) =>
-        {
-            var i = 0;
-#pragma warning disable SA1312
-            await foreach (var _ in nats.SubscribeAsync<string>("foo.*"))
-#pragma warning restore SA1312
-            {
-                await channelWriter.WriteAsync(new object());
-                if (++i >= iterations)
-                    break;
-            }
-        });
-
-    [Test]
-    public void Subscription_should_not_be_collected_subscribe_core_async_read_all_async() =>
-        RunSubTest(async (nats, channelWriter, iterations) =>
-        {
-            var i = 0;
-            await using var sub = await nats.SubscribeCoreAsync<string>("foo.*");
-#pragma warning disable SA1312
-            await foreach (var _ in sub.Msgs.ReadAllAsync())
-#pragma warning restore SA1312
-            {
-                await channelWriter.WriteAsync(new object());
-                if (++i >= iterations)
-                    break;
-            }
-        });
-
-    [Test]
-    public void Subscription_should_not_be_collected_subscribe_core_async_read_async() =>
-        RunSubTest(async (nats, channelWriter, iterations) =>
-        {
-            var i = 0;
-            await using var sub = await nats.SubscribeCoreAsync<string>("foo.*");
-            while (true)
-            {
-                await sub.Msgs.ReadAsync();
-                await channelWriter.WriteAsync(new object());
-                if (++i >= iterations)
-                    break;
-            }
-        });
-
-    [Test]
-    public void Subscription_should_not_be_collected_subscribe_core_async_wait_to_read_async() =>
-        RunSubTest(async (nats, channelWriter, iterations) =>
-        {
-            var i = 0;
-            await using var sub = await nats.SubscribeCoreAsync<string>("foo.*");
-            while (await sub.Msgs.WaitToReadAsync())
-            {
-                while (sub.Msgs.TryRead(out _))
-                {
-                    await channelWriter.WriteAsync(new object());
-                    i++;
-                }
-
-                if (i >= iterations)
-                {
-                    break;
-                }
-            }
-        });
-
-    private void RunSubTest(Func<NatsConnection, ChannelWriter<object>, int, Task> subTask)
+    public void Subscription_should_not_be_collected_subscribe_async()
     {
         var server = NatsServer.Start();
         try
         {
             const int iterations = 10;
+            const string subject = "foo.data";
             var nats = server.CreateClientConnection(new NatsOpts { RequestTimeout = TimeSpan.FromSeconds(10) });
             var received = Channel.CreateUnbounded<object>();
-            var task = subTask(nats, received.Writer, iterations);
 
-            var i = 0;
-            var fail = 0;
-            while (true)
+            var subTask = Task.Run(async () =>
             {
-                nats.PublishAsync("foo.data", "data").AsTask().GetAwaiter().GetResult();
-                try
+                var i = 0;
+#pragma warning disable SA1312
+                await foreach (var _ in nats.SubscribeAsync<string>(subject))
+#pragma warning restore SA1312
                 {
-                    using var cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(100));
-                    received.Reader.ReadAsync(cts.Token).AsTask().GetAwaiter().GetResult();
+                    await received.Writer.WriteAsync(new object());
+                    if (++i >= iterations)
+                        break;
                 }
-                catch (OperationCanceledException)
+            });
+
+            RunSubTest(iterations, subject, nats, received, subTask);
+        }
+        finally
+        {
+            server.DisposeAsync().AsTask().GetAwaiter().GetResult();
+        }
+    }
+
+    [Test]
+    public void Subscription_should_not_be_collected_subscribe_core_async_read_all_async()
+    {
+        var server = NatsServer.Start();
+        try
+        {
+            const int iterations = 10;
+            const string subject = "foo.data";
+            var nats = server.CreateClientConnection(new NatsOpts { RequestTimeout = TimeSpan.FromSeconds(10) });
+            var received = Channel.CreateUnbounded<object>();
+
+            var subTask = Task.Run(async () =>
+            {
+                var i = 0;
+                await using var sub = await nats.SubscribeCoreAsync<string>(subject);
+#pragma warning disable SA1312
+                await foreach (var _ in sub.Msgs.ReadAllAsync())
+#pragma warning restore SA1312
                 {
-                    if (++fail <= 10)
+                    await received.Writer.WriteAsync(new object());
+                    if (++i >= iterations)
+                        break;
+                }
+            });
+
+            RunSubTest(iterations, subject, nats, received, subTask);
+        }
+        finally
+        {
+            server.DisposeAsync().AsTask().GetAwaiter().GetResult();
+        }
+    }
+
+    [Test]
+    public void Subscription_should_not_be_collected_subscribe_core_async_read_async()
+    {
+        var server = NatsServer.Start();
+        try
+        {
+            const int iterations = 10;
+            const string subject = "foo.data";
+            var nats = server.CreateClientConnection(new NatsOpts { RequestTimeout = TimeSpan.FromSeconds(10) });
+            var received = Channel.CreateUnbounded<object>();
+
+            var subTask = Task.Run(async () =>
+            {
+                var i = 0;
+                await using var sub = await nats.SubscribeCoreAsync<string>(subject);
+                while (true)
+                {
+                    await sub.Msgs.ReadAsync();
+                    await received.Writer.WriteAsync(new object());
+                    if (++i >= iterations)
+                        break;
+                }
+            });
+
+            RunSubTest(iterations, subject, nats, received, subTask);
+        }
+        finally
+        {
+            server.DisposeAsync().AsTask().GetAwaiter().GetResult();
+        }
+    }
+
+    [Test]
+    public void Subscription_should_not_be_collected_subscribe_core_async_wait_to_read_async()
+    {
+        var server = NatsServer.Start();
+        try
+        {
+            const int iterations = 10;
+            const string subject = "foo.data";
+            var nats = server.CreateClientConnection(new NatsOpts { RequestTimeout = TimeSpan.FromSeconds(10) });
+            var received = Channel.CreateUnbounded<object>();
+
+            var subTask = Task.Run(async () =>
+            {
+                var i = 0;
+                await using var sub = await nats.SubscribeCoreAsync<string>(subject);
+                while (await sub.Msgs.WaitToReadAsync())
+                {
+                    while (sub.Msgs.TryRead(out _))
                     {
-                        continue;
+                        await received.Writer.WriteAsync(new object());
+                        i++;
                     }
 
-                    Assert.Fail($"failed to receive a reply 10 times");
+                    if (i >= iterations)
+                    {
+                        break;
+                    }
+                }
+            });
+
+            RunSubTest(iterations, subject, nats, received, subTask);
+        }
+        finally
+        {
+            server.DisposeAsync().AsTask().GetAwaiter().GetResult();
+        }
+    }
+
+    private void RunSubTest(int iterations, string subject, NatsConnection nats, Channel<object> received, Task subTask)
+    {
+        var i = 0;
+        var fail = 0;
+        while (true)
+        {
+            nats.PublishAsync(subject, "data").AsTask().GetAwaiter().GetResult();
+            try
+            {
+                using var cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(100));
+                received.Reader.ReadAsync(cts.Token).AsTask().GetAwaiter().GetResult();
+            }
+            catch (OperationCanceledException)
+            {
+                if (++fail <= 10)
+                {
+                    continue;
                 }
 
-                if (++i >= iterations)
-                    break;
-
-                GC.Collect();
-                dotMemory.Check(memory =>
-                {
-                    var count = memory.GetObjects(where => where.Type.Is<NatsSub<string>>()).ObjectsCount;
-                    Assert.That(count, Is.EqualTo(1), $"Alive - received {i}");
-                });
+                Assert.Fail($"failed to receive a reply 10 times");
             }
 
-            task.GetAwaiter().GetResult();
+            if (++i >= iterations)
+                break;
 
             GC.Collect();
             dotMemory.Check(memory =>
             {
                 var count = memory.GetObjects(where => where.Type.Is<NatsSub<string>>()).ObjectsCount;
-                Assert.That(count, Is.EqualTo(0), "Collected");
+                Assert.That(count, Is.EqualTo(1), $"Alive - received {i}");
             });
         }
-        finally
+
+        subTask.GetAwaiter().GetResult();
+
+        GC.Collect();
+        dotMemory.Check(memory =>
         {
-            server.DisposeAsync().GetAwaiter().GetResult();
-        }
+            var count = memory.GetObjects(where => where.Type.Is<NatsSub<string>>()).ObjectsCount;
+            Assert.That(count, Is.EqualTo(0), "Collected");
+        });
     }
 }
