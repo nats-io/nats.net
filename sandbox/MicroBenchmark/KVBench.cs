88ae07e1b26779c05cde5cd8002320a455ebd401
Ziya Suzen <ziya@suzen.net>
2024-12-05 16:56:23 +0000

    Add bench

--- sandbox/MicroBenchmark/KVBench.cs
+++ sandbox/MicroBenchmark/KVBench.cs
@@ -66,4 +66,53 @@ public class KVBench
 
         return 0;
     }
+
+    [Benchmark]
+    public async ValueTask<int> TryGetMultiAsync()
+    {
+        List<Task> tasks = new();
+        for (var i = 0; i < 100; i++)
+        {
+            tasks.Add(Task.Run(async () =>
+            {
+                var result = await _store.TryGetEntryAsync<int>("does.not.exist");
+                if (result is { Success: false, Error: NatsKVKeyNotFoundException })
+                {
+                    return 1;
+                }
+
+                return 0;
+            }));
+        }
+
+        await Task.WhenAll(tasks);
+
+        return 0;
+    }
+
+    [Benchmark]
+    public async ValueTask<int> GetMultiAsync()
+    {
+        List<Task> tasks = new();
+        for (var i = 0; i < 100; i++)
+        {
+            tasks.Add(Task.Run(async () =>
+            {
+                try
+                {
+                    await _store.GetEntryAsync<int>("does.not.exist");
+                }
+                catch (NatsKVKeyNotFoundException)
+                {
+                    return 1;
+                }
+
+                return 0;
+            }));
+        }
+
+        await Task.WhenAll(tasks);
+
+        return 0;
+    }
 }
