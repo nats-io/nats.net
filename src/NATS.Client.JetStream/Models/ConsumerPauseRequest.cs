e899639afc06f4fd51c74a759afc007e3528fb53
mtmk <ziya@synadia.com>
2025-02-19 20:05:04 +0000

    Consumer pause docs update (#742)

--- src/NATS.Client.JetStream/Models/ConsumerPauseRequest.cs
+++ src/NATS.Client.JetStream/Models/ConsumerPauseRequest.cs
@@ -3,6 +3,7 @@ namespace NATS.Client.JetStream.Models;
 /// <summary>
 /// A request to the JetStream $JS.API.CONSUMER.PAUSE API
 /// </summary>
+/// <remarks>This feature is only available on NATS server v2.11 and later.</remarks>
 internal record ConsumerPauseRequest
 {
     [System.Text.Json.Serialization.JsonPropertyName("pause_until")]
