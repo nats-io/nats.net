2858c34e606c3a9901f32d80455ca39fade91248
Kumakhov Kasbolat <uhfath@gmail.com>
2025-03-03 20:17:35 +0300

    Added an ability to specify pool size with a callback using IServiceProvider

--- tests/NATS.Extensions.Microsoft.DependencyInjection.Tests/NatsHostingExtensionsTests.cs
+++ tests/NATS.Extensions.Microsoft.DependencyInjection.Tests/NatsHostingExtensionsTests.cs
@@ -178,6 +178,21 @@ public class NatsHostingExtensionsTests
     }
 
     [Fact]
+    public void AddNatsClient_RegistersNatsConnectionAsTransient_WhenPoolSizeFuncIsGreaterThanOne()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton<ILoggerFactory, NullLoggerFactory>();
+        services.AddNatsClient(nats => nats.WithPoolSize(_ => 2));
+
+        var provider = services.BuildServiceProvider();
+        var natsConnection1 = provider.GetRequiredService<INatsConnection>();
+        var natsConnection2 = provider.GetRequiredService<INatsConnection>();
+
+        Assert.NotNull(natsConnection1);
+        Assert.NotSame(natsConnection1, natsConnection2); // Transient should return different instances
+    }
+
+    [Fact]
     public async Task AddNatsClient_WithJsonSerializer()
     {
         await using var server = await NatsServer.StartAsync();
@@ -309,6 +324,56 @@ public class NatsHostingExtensionsTests
 
         services.AddNatsClient(builder => builder.WithPoolSize(2).WithKey(key1));
         services.AddNatsClient(builder => builder.WithPoolSize(2).WithKey(key2));
+        var provider = services.BuildServiceProvider();
+
+        Dictionary<string, List<object>> connections = new();
+        foreach (var key in new[] { key1, key2 })
+        {
+            var nats1 = provider.GetKeyedService<INatsConnection>(key);
+            Assert.NotNull(nats1);
+            var nats2 = provider.GetKeyedService<INatsConnection>(key);
+            Assert.NotNull(nats2);
+            var nats3 = provider.GetKeyedService<INatsConnection>(key);
+            Assert.NotNull(nats3);
+            var nats4 = provider.GetKeyedService<INatsConnection>(key);
+            Assert.NotNull(nats4);
+
+            // relying on the fact that the pool size is 2 and connections are returned in a round-robin fashion
+            Assert.NotSame(nats1, nats2);
+            Assert.Same(nats1, nats3);
+            Assert.NotSame(nats2, nats3);
+            Assert.Same(nats2, nats4);
+
+            if (!connections.TryGetValue(key, out var list))
+            {
+                list = new List<object>();
+                connections.Add(key, list);
+            }
+
+            list.Add(nats1);
+            list.Add(nats2);
+            list.Add(nats3);
+            list.Add(nats4);
+        }
+
+        foreach (var obj1 in connections[key1])
+        {
+            foreach (var obj2 in connections[key2])
+                Assert.NotSame(obj1, obj2);
+        }
+    }
+
+    [Fact]
+    public void AddNats_RegistersKeyedNatsConnection_WhenKeyIsProvided_pooledFunc()
+    {
+        var key1 = "TestKey1";
+        var key2 = "TestKey2";
+
+        var services = new ServiceCollection();
+        services.AddSingleton<ILoggerFactory, NullLoggerFactory>();
+
+        services.AddNatsClient(builder => builder.WithPoolSize(_ => 2).WithKey(key1));
+        services.AddNatsClient(builder => builder.WithPoolSize(_ => 2).WithKey(key2));
         var provider = services.BuildServiceProvider();
 
         Dictionary<string, List<object>> connections = new();
