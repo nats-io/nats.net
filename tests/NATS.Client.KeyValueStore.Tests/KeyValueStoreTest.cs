f23fb8479b923b2c796e6beb5fb1ae81a11e0955
Ziya Suzen <ziya@suzen.net>
2025-03-10 10:18:10 +0000

    Add test

--- tests/NATS.Client.KeyValueStore.Tests/KeyValueStoreTest.cs
+++ tests/NATS.Client.KeyValueStore.Tests/KeyValueStoreTest.cs
@@ -515,7 +515,13 @@ public class KeyValueStoreTest
         var js = new NatsJSContext(nats);
         var kv = new NatsKVContext(js);
 
-        var store = await kv.CreateStoreAsync(new NatsKVConfig("kv1") { History = 10 }, cancellationToken: cancellationToken);
+        var store = await kv.CreateStoreAsync(
+            new NatsKVConfig("kv1")
+            {
+                History = 10,
+                Metadata = new Dictionary<string, string> { { "meta1", "value1" } },
+            },
+            cancellationToken: cancellationToken);
 
         Assert.Equal("kv1", store.Bucket);
 
@@ -524,6 +530,7 @@ public class KeyValueStoreTest
         Assert.Equal("kv1", status.Bucket);
         Assert.Equal("KV_kv1", status.Info.Config.Name);
         Assert.Equal(10, status.Info.Config.MaxMsgsPerSubject);
+        Assert.Equal("value1", status.Info.Config.Metadata?["meta1"]);
     }
 
     [SkipIfNatsServer(versionEarlierThan: "2.10")]
