58919bb726d93036743bf496dd643fb7d5fd0238
Ziya Suzen <ziya@suzen.net>
2025-04-10 16:06:23 +0100

    Fix test

--- tests/NATS.Client.JetStream.Tests/PublishRetryTest.cs
+++ tests/NATS.Client.JetStream.Tests/PublishRetryTest.cs
@@ -5,36 +5,31 @@ using NATS.Net;
 
 namespace NATS.Client.JetStream.Tests;
 
-[Collection("nats-server-restricted-user")]
+[Collection("nats-server")]
 public class PublishRetryTest
 {
     private readonly ITestOutputHelper _output;
-    private readonly NatsServerRestrictedUserFixture _server;
-    private readonly bool _isFramework;
+    private readonly NatsServerFixture _server;
 
-    public PublishRetryTest(ITestOutputHelper output, NatsServerRestrictedUserFixture server)
+    public PublishRetryTest(ITestOutputHelper output, NatsServerFixture server)
     {
         _output = output;
         _server = server;
-        _isFramework = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.Contains("Framework");
     }
 
     [Fact]
     public async Task Publish_without_telemetry()
     {
         var cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
-        await using var nats = new NatsConnection(new NatsOpts
-        {
-            Url = _server.Url,
-            AuthOpts = new NatsAuthOpts { Username = "u" },
-        });
+        await using var nats = new NatsConnection(new NatsOpts { Url = _server.Url });
         var prefix = _server.GetNextId();
 
         // Without telemetry
         {
             var headers = new NatsHeaders();
             await nats.PublishAsync(prefix, headers: headers, cancellationToken: cts.Token);
-            Assert.DoesNotContain(_isFramework ? "Request-Id" : "traceparent", headers);
+            Assert.DoesNotContain("Request-Id", headers);
+            Assert.DoesNotContain("traceparent", headers);
             Assert.Empty(headers);
         }
 
@@ -42,7 +37,8 @@ public class PublishRetryTest
         {
             var headers = new NatsHeaders();
             await nats.PublishAsync(prefix, data: 1, headers: headers, cancellationToken: cts.Token);
-            Assert.DoesNotContain(_isFramework ? "Request-Id" : "traceparent", headers);
+            Assert.DoesNotContain("Request-Id", headers);
+            Assert.DoesNotContain("traceparent", headers);
             Assert.Empty(headers);
         }
     }
