1f1fc1b605c3813b3ff50963f6ae057c8ef569a8
James Thompson <thompson.tomo@outlook.com>
2025-05-22 10:38:59 +1000

    Tweaks
    Signed-off-by: James Thompson <thompson.tomo@outlook.com>

---
+++ src/NATS.Client.Abstractions/INatsSerialize.cs
@@ -0,0 +1,50 @@
+namespace NATS.Client.Core;
+
+/// <summary>
+/// Serializer interface for NATS messages.
+/// </summary>
+/// <typeparam name="T">Serialized object type</typeparam>
+public interface INatsSerializer<T> : INatsSerialize<T>, INatsDeserialize<T>
+{
+    /// <summary>
+    /// Combines the current serializer with the specified serializer.
+    /// </summary>
+    /// <param name="next">The serializer to be combined with.</param>
+    /// <returns>The combined serializer.</returns>
+    INatsSerializer<T> CombineWith(INatsSerializer<T> next);
+}
+
+/// <summary>
+/// Serializer interface for NATS messages.
+/// </summary>
+/// <typeparam name="T">Serialized object type</typeparam>
+public interface INatsSerialize<in T>
+{
+    /// <summary>
+    /// Serialize value to buffer.
+    /// </summary>
+    /// <param name="bufferWriter">Buffer to write the serialized data.</param>
+    /// <param name="value">Object to be serialized.</param>
+    void Serialize(IBufferWriter<byte> bufferWriter, T value);
+}
+
+/// <summary>
+/// Deserializer interface for NATS messages.
+/// </summary>
+/// <typeparam name="T">Deserialized object type</typeparam>
+public interface INatsDeserialize<out T>
+{
+    /// <summary>
+    /// Deserialize value from buffer.
+    /// </summary>
+    /// <param name="buffer">Buffer with the serialized data.</param>
+    /// <returns>Deserialized object</returns>
+    T? Deserialize(in ReadOnlySequence<byte> buffer);
+}
+
+public interface INatsSerializerRegistry
+{
+    INatsSerialize<T> GetSerializer<T>();
+
+    INatsDeserialize<T> GetDeserializer<T>();
+}
