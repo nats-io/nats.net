ce91fe610e6745faee9fe88b0caf275a706c64ff
Adam Gigi <44773238+AdamSendible@users.noreply.github.com>
2025-06-30 15:36:07 +0100

    Refactor NatsBuilder to use OptionsBuilder

--- src/NATS.Extensions.Microsoft.DependencyInjection/NatsBuilder.cs
+++ src/NATS.Extensions.Microsoft.DependencyInjection/NatsBuilder.cs
@@ -4,24 +4,45 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
 using NATS.Client.Core;
 using NATS.Net;
 
 namespace NATS.Extensions.Microsoft.DependencyInjection;
+
+public class NatsOptsBuilder
+{
+    public NatsOpts Opts { get; set; } = NatsOpts.Default;
+
+    public Action<IServiceProvider, NatsConnection>? ConfigureConnection { get; set; }
+}
 
 public class NatsBuilder
 {
     private readonly IServiceCollection _services;
 
     private Func<IServiceProvider, int> _poolSizeConfigurer = _ => 1;
-    private Func<IServiceProvider, NatsOpts, NatsOpts>? _configureOpts;
-    private Action<IServiceProvider, NatsConnection>? _configureConnection;
     private object? _diKey = null;
-    private BoundedChannelFullMode? _pending = null;
-    private INatsSerializerRegistry? _serializerRegistry = null;
 
     public NatsBuilder(IServiceCollection services)
-        => _services = services;
+    {
+        _services = services;
+        _services
+            .AddOptions<NatsOptsBuilder>()
+            .Configure<IServiceProvider>((opts, provider) =>
+            {
+                opts.Opts = opts.Opts with
+                {
+                    LoggerFactory = provider.GetService<ILoggerFactory>() ?? NullLoggerFactory.Instance,
+                    SubPendingChannelFullMode = BoundedChannelFullMode.Wait,
+                };
+
+                if (ReferenceEquals(opts.Opts.SerializerRegistry, NatsOpts.Default.SerializerRegistry))
+                {
+                    opts.Opts = opts.Opts with { SerializerRegistry = NatsClientDefaultSerializerRegistry.Default, };
+                }
+            });
+    }
 
     public NatsBuilder WithPoolSize(int size)
     {
@@ -37,51 +58,44 @@ public class NatsBuilder
         return this;
     }
 
-    public NatsBuilder ConfigureOptions(Func<NatsOpts, NatsOpts> optsFactory) =>
-        ConfigureOptions((_, opts) => optsFactory(opts));
-
-    public NatsBuilder ConfigureOptions(Func<IServiceProvider, NatsOpts, NatsOpts> optsFactory)
+    public NatsBuilder ConfigureOptions(Action<OptionsBuilder<NatsOptsBuilder>> builder)
     {
-        var configure = _configureOpts;
-        _configureOpts = (serviceProvider, opts) =>
-        {
-            opts = configure?.Invoke(serviceProvider, opts) ?? opts;
-
-            return optsFactory(serviceProvider, opts);
-        };
+        builder(_services.AddOptions<NatsOptsBuilder>());
 
         return this;
     }
+
+    [Obsolete("Use ConfigureOptions(Action<OptionsBuilder<NatsOptsBuilder>> builder) instead.")]
+    public NatsBuilder ConfigureOptions(Func<NatsOpts, NatsOpts> optsFactory) =>
+        ConfigureOptions(builder => builder.Configure(opts =>
+            opts.Opts = optsFactory(opts.Opts)));
+
+    [Obsolete("Use ConfigureOptions(Action<OptionsBuilder<NatsOptsBuilder>> builder) instead.")]
+    public NatsBuilder ConfigureOptions(Func<IServiceProvider, NatsOpts, NatsOpts> optsFactory) =>
+        ConfigureOptions(builder => builder.Configure<IServiceProvider>((opts, provider) =>
+            opts.Opts = optsFactory(provider, opts.Opts)));
+
+    public NatsBuilder ConfigureConnection(Action<IServiceProvider, NatsConnection> configureConnection) =>
+        ConfigureOptions(builder => builder.Configure<IServiceProvider>((opts, provider) =>
+            opts.ConfigureConnection = configureConnection));
 
     public NatsBuilder ConfigureConnection(Action<NatsConnection> configureConnection) =>
-        ConfigureConnection((_, con) => configureConnection(con));
-
-    public NatsBuilder ConfigureConnection(Action<IServiceProvider, NatsConnection> configureConnection)
-    {
-        var configure = _configureConnection;
-        _configureConnection = (serviceProvider, connection) =>
-        {
-            configure?.Invoke(serviceProvider, connection);
-
-            configureConnection(serviceProvider, connection);
-        };
-
-        return this;
-    }
+        ConfigureConnection((_, connection) => configureConnection(connection));
 
     [Obsolete("By default JSON serialization is enabled. If you still want to use generated JSON serialization, use WithSerializerRegistry, JsonSerializerContext and optionally NatsSerializerBuilder")]
     public NatsBuilder AddJsonSerialization(JsonSerializerContext context) =>
         AddJsonSerialization(_ => context);
 
     [Obsolete("By default JSON serialization is enabled. If you still want to use generated JSON serialization, use WithSerializerRegistry, JsonSerializerContext and optionally NatsSerializerBuilder")]
-    public NatsBuilder AddJsonSerialization(Func<IServiceProvider, JsonSerializerContext> contextFactory)
-        => ConfigureOptions((serviceProvider, opts) =>
-        {
-            var context = contextFactory(serviceProvider);
-            NatsJsonContextSerializerRegistry jsonRegistry = new(context);
+    public NatsBuilder AddJsonSerialization(Func<IServiceProvider, JsonSerializerContext> contextFactory) =>
+        ConfigureOptions(builder =>
+            builder.Configure<IServiceProvider>((opts, provider) =>
+            {
+                var context = contextFactory(provider);
+                NatsJsonContextSerializerRegistry jsonRegistry = new(context);
 
-            return opts with { SerializerRegistry = jsonRegistry };
-        });
+                opts.Opts = opts.Opts with { SerializerRegistry = jsonRegistry };
+            }));
 
 #if NET8_0_OR_GREATER
     public NatsBuilder WithKey(object key)
@@ -102,11 +116,11 @@ public class NatsBuilder
     /// By default, the pending messages channel will wait for space to be available when full.
     /// Note that this is not the same as <c>NatsOpts</c> default <c>SubPendingChannelFullMode</c> which is <c>DropNewest</c>.
     /// </remarks>
-    public NatsBuilder WithSubPendingChannelFullMode(BoundedChannelFullMode pending)
-    {
-        _pending = pending;
-        return this;
-    }
+    public NatsBuilder WithSubPendingChannelFullMode(BoundedChannelFullMode pending) =>
+        ConfigureOptions(builder => builder.Configure(opts => opts.Opts = opts.Opts with
+        {
+            SubPendingChannelFullMode = pending,
+        }));
 
     /// <summary>
     /// Override the default <see cref="INatsSerializerRegistry"/> for the options.
@@ -119,11 +133,11 @@ public class NatsBuilder
     /// Note that this is not the same as <c>NatsOpts</c> default <c>SerializerRegistry</c> which
     /// doesn't do ad-hoc JSON serialization.
     /// </remarks>
-    public NatsBuilder WithSerializerRegistry(INatsSerializerRegistry registry)
-    {
-        _serializerRegistry = registry;
-        return this;
-    }
+    public NatsBuilder WithSerializerRegistry(INatsSerializerRegistry registry) =>
+        ConfigureOptions(builder => builder.Configure(opts => opts.Opts = opts.Opts with
+        {
+            SerializerRegistry = registry,
+        }));
 
     internal IServiceCollection Build()
     {
@@ -164,30 +178,8 @@ public class NatsBuilder
 
     private NatsConnectionPool PoolFactory(IServiceProvider provider, object? diKey = null)
     {
-        var options = GetNatsOpts(provider);
+        var options = provider.GetRequiredService<IOptions<NatsOptsBuilder>>().Value;
 
-        return new NatsConnectionPool(_poolSizeConfigurer(provider), options, con => _configureConnection?.Invoke(provider, con));
-    }
-
-    private NatsOpts GetNatsOpts(IServiceProvider provider)
-    {
-        var options = NatsOpts.Default with { LoggerFactory = provider.GetService<ILoggerFactory>() ?? NullLoggerFactory.Instance };
-        options = _configureOpts?.Invoke(provider, options) ?? options;
-
-        if (_serializerRegistry != null)
-        {
-            options = options with { SerializerRegistry = _serializerRegistry };
-        }
-        else
-        {
-            if (ReferenceEquals(options.SerializerRegistry, NatsOpts.Default.SerializerRegistry))
-            {
-                options = options with { SerializerRegistry = NatsClientDefaultSerializerRegistry.Default, };
-            }
-        }
-
-        options = options with { SubPendingChannelFullMode = _pending ?? BoundedChannelFullMode.Wait };
-
-        return options;
+        return new NatsConnectionPool(_poolSizeConfigurer(provider), options.Opts, con => options.ConfigureConnection?.Invoke(provider, con));
     }
 }
