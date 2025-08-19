bfe3a6fc7adc5e139f5b5cb23e8dc65b74d9d2a2
Ziya Suzen <ziya@suzen.net>
2025-08-19 10:28:12 +0100

    Add reconnect tests for handling max connections

---
+++ tests/NATS.Client.Core2.Tests/ReconnectTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Logging;
+using NATS.Client.Core.Tests;
+using NATS.Client.Platform.Windows.Tests;
+using NATS.Client.TestUtilities;
+
+namespace NATS.Client.Core2.Tests;
+
+public class ReconnectTests(ITestOutputHelper output)
+{
+    [Fact]
+    public async Task Reconnect_on_max_connections()
+    {
+        var logger = new InMemoryTestLoggerFactory(LogLevel.Warning, m => output.WriteLine($"LOG: {m.Message}"));
+        var confFile = $"{nameof(Reconnect_on_max_connections)}_server.conf";
+        {
+            var config = """
+                         accounts: {
+                           $SYS { users [{user: sys, password: sys}]}
+                           test { users [{user: foo, password: bar}]}
+                         }
+                         max_connections: 2
+                         """;
+            File.WriteAllText(path: confFile, contents: config);
+        }
+
+        await using var server = await NatsServerProcess.StartAsync(config: confFile, withJs: false);
+
+        await using var nats1 = new NatsConnection(new NatsOpts { Url = server.Url, LoggerFactory = logger, AuthOpts = new NatsAuthOpts { Username = "sys", Password = "sys" } });
+        await nats1.ConnectAsync();
+
+        await using var nats2 = new NatsConnection(new NatsOpts { Url = server.Url, LoggerFactory = logger, AuthOpts = new NatsAuthOpts { Username = "sys", Password = "sys" } });
+        await nats2.ConnectAsync();
+
+        await using var nats3 = new NatsConnection(new NatsOpts { Url = server.Url, AuthOpts = new NatsAuthOpts { Username = "sys", Password = "sys" } });
+        await Assert.ThrowsAsync<NatsException>(async () => await nats3.ConnectAsync());
+
+        // Reload the server configuration to change max connections to 1
+        {
+            var config = """
+                         accounts: {
+                           $SYS { users [{user: sys, password: sys}]}
+                         }
+                         max_connections: 1
+                         """;
+            File.WriteAllText(path: confFile, contents: config);
+        }
+
+        _ = Task.Run(async () => await nats1.RequestAsync<string>($"$SYS.REQ.SERVER.{nats1.ServerInfo!.Id}.RELOAD"));
+
+        await Retry.Until(
+            "warning",
+            () => logger.Logs.Any(l => l.Message.Contains("maximum connections exceeded")));
+
+        // Reload the server configuration to change max connections back to 2
+        {
+            var config = """
+                         accounts: {
+                           $SYS { users [{user: sys, password: sys}]}
+                         }
+                         max_connections: 2
+                         """;
+            File.WriteAllText(path: confFile, contents: config);
+        }
+
+        _ = Task.Run(async () => await nats1.RequestAsync<string>($"$SYS.REQ.SERVER.{nats1.ServerInfo!.Id}.RELOAD"));
+        _ = Task.Run(async () => await nats2.RequestAsync<string>($"$SYS.REQ.SERVER.{nats2.ServerInfo!.Id}.RELOAD"));
+
+        await Retry.Until(
+            "both connections are open",
+            () => nats1.ConnectionState == NatsConnectionState.Open && nats2.ConnectionState == NatsConnectionState.Open);
+    }
+}
