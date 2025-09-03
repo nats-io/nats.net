f69c1c32e7c3e142fef118bf12d9bcddec938a34
Ziya Suzen <ziya@suzen.net>
2025-09-03 11:17:07 +0100

    Add missing raft_group to cluster info

--- src/NATS.Client.JetStream/Models/ClusterInfo.cs
+++ src/NATS.Client.JetStream/Models/ClusterInfo.cs
@@ -10,6 +10,13 @@ public record ClusterInfo
     public string? Name { get; set; }
 
     /// <summary>
+    /// RAFT group name
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("raft_group")]
+    [System.Text.Json.Serialization.JsonIgnore(Condition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingDefault)]
+    public string? RaftGroup { get; set; }
+
+    /// <summary>
     /// The server name of the RAFT leader
     /// </summary>
     [System.Text.Json.Serialization.JsonPropertyName("leader")]
@@ -22,4 +29,9 @@ public record ClusterInfo
     [System.Text.Json.Serialization.JsonPropertyName("replicas")]
     [System.Text.Json.Serialization.JsonIgnore(Condition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingDefault)]
     public ICollection<PeerInfo>? Replicas { get; set; }
+
+    // TODO: These are 2.12 specific
+    // LeaderSince *time.Time  `json:"leader_since,omitempty"`
+    // SystemAcc   bool        `json:"system_account,omitempty"`
+    // TrafficAcc  string      `json:"traffic_account,omitempty"`
 }
