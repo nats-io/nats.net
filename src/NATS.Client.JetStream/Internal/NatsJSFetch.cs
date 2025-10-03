bc1aa67d77c8cf9e3a0960e0c4f33804bcf5b0c4
Arad Alvand <aradalvand@gmail.com>
2025-10-03 09:48:52 -0400

    Remove redundant `Console.WriteLine` that was put there for testing

--- src/NATS.Client.JetStream/Internal/NatsJSFetch.cs
+++ src/NATS.Client.JetStream/Internal/NatsJSFetch.cs
@@ -242,7 +242,6 @@ internal class NatsJSFetch<TMsg> : NatsSubBase
                     }
                     else
                     {
-                        Console.WriteLine("HOLY SHIT");
                         _notificationChannel?.Notify(new NatsJSProtocolNotification("Unhandled protocol message", headers.Code, headers.MessageText));
                         _logger.LogWarning(NatsJSLogEvents.ProtocolMessage, "Unhandled protocol message: {Code} {Description}", headers.Code, headers.MessageText);
                     }
